3725e0831b1851e25d67de92e94356de10026961
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-24 10:41:14 +0000

    Added a skeleton TDD test file, so that I can dynamically fix the broken class.

---
+++ static_and_dynamic_test/testing_task_2_spec.rb
@@ -0,0 +1,14 @@
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('testing_task_2.rb')
+
+class CardGameTest < MiniTest::Test
+
+  def setup
+    @card1 = CardGame.new("Spades", 3)
+    @card2 = CardGame.new("Hearts", 9)
+    @card3 = CardGame.new("Diamonds", 1)
+  end
+
+end
