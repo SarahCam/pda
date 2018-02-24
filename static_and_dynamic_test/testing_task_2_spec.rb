14970f7b2334396fe7765b60a38d6b5359aeb961
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-24 11:07:08 +0000

    Added a TDD test to check the 'highest_card', where the highest card is the second card parameter supplied.  The test fails presently as it does not recognise the highest_card method.

--- static_and_dynamic_test/testing_task_2_spec.rb
+++ static_and_dynamic_test/testing_task_2_spec.rb
@@ -27,4 +27,12 @@ class CardGameTest < MiniTest::Test
     assert_equal(true, CardGame.checkforAce(@card3))
   end
 
+  def test_highest_card___9_of_Hearts___card2
+    assert_equal(@card2, CardGame.highest_card(@card1, @card2))
+  end
+
+  # def test_highest_card___9_of_Hearts___card2
+  #   assert_equal(@card2, CardGame.highest_card(@card2, @card3))
+  # end
+
 end
